8499ad646caa12cc4c861e4caa4b30b5e72ba48d
= <=>
2018-09-16 12:22:30 +0700

    commit 2

---
+++ js/test.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:5,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2
+        }
+    }
+})
+});
